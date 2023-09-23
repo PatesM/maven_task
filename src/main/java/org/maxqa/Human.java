3d89958735fdcd5283e96ac86aa63957777cd8f7
MaximP <maximpates@gmail.com>
2023-09-23 12:03:17 +0500

    Added solution of the task

---
+++ src/main/java/org/maxqa/Human.java
@@ -0,0 +1,14 @@
+package org.maxqa;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class Human {
+
+  String firstName;
+  String lastName;
+}
---
+++ src/main/java/org/maxqa/Main.java
@@ -0,0 +1,10 @@
+package org.maxqa;
+
+public class Main {
+
+  public static void main(String[] args) {
+
+    Human human1 = new Human("Maxim", "Pates");
+    System.out.println(human1);
+  }
+}
